65c5b5974d9da3181d432ff179a880d2b025f1c3
Muhammad Qasim Akram <m.qasimakram46@gmail.com>
2026-04-12 16:59:51 +0500

    now using local storage

--- chatbot-project/src/App.jsx
+++ chatbot-project/src/App.jsx
@@ -1,47 +1,41 @@
-import { useState} from 'react'
+import { useEffect, useState } from 'react'
 import { ChatInput } from './components/ChatInput';
 import ChatMessages from './components/ChatMessages';
 import './App.css'
 
-     
+const defaultMessages = [
+    { message: "Hello Chatbot", sender: "user", id: "0" },
+    { message: "Hello! How can i help you", sender: "robot", id: "1" },
+    { message: "can you get me todays date?", sender: "user", id: "2" },
+    { message: "Today is September 27", sender: "robot", id: "3" }
+]
 
- function App() {
-            const [chatMessages, setChatMessages] = useState([
-                {
-                    message: "Hello Chatbot",
-                    sender: "user",
-                    id: "0"
-                },
-                {
-                    message: "Hello! How can i help you",
-                    sender: "robot",
-                    id: "1"
-                },
-                {
-                    message: "can you get me todays date?",
-                    sender: "user",
-                    id: "2"
-                },
-                {
-                    message: "Today is September 27",
-                    sender: "robot",
-                    id: "3"
-                }])
+function App() {
+    const [chatMessages, setChatMessages] = useState(
+        JSON.parse(localStorage.getItem('messages')) ||
+        []
+    )
 
-            return (
-                <div className="app-container">
+    useEffect(
+        () => {
+            localStorage.setItem('messages', JSON.stringify(chatMessages))
+        },
+        [chatMessages]
+    )
+    
+    return (
+        <div className="app-container">
 
+            <ChatMessages
+                chatMessages={[...defaultMessages, ...chatMessages]}
+            />
+            <ChatInput
+                chatMessages={chatMessages}
+                setChatMessages={setChatMessages}
+            />
 
-                    <ChatMessages
-                        chatMessages={chatMessages}
-                    />
-                    <ChatInput
-                        chatMessages={chatMessages}
-                        setChatMessages={setChatMessages}
-                    />
-
-                </div>
-            )
-        }
+        </div>
+    )
+}
 
 export default App
