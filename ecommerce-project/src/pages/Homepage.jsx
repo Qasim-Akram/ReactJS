bcd1638f6abf45d2c3014c07741f9c11f22ba7da
Muhammad Qasim Akram <m.qasimakram46@gmail.com>
2026-04-25 09:06:34 +0500

    using axios for fetching

--- ecommerce-project/src/pages/Homepage.jsx
+++ ecommerce-project/src/pages/Homepage.jsx
@@ -1,15 +1,14 @@
+import axios from 'axios';
 import './Homepage.css'
 import { Header } from '../components/Header';
 import { products } from '../../starting-code/data/products';
 
 export function HomePage() {
-    fetch('http://localhost:3000/api/products').
-        then((response) => {
-            return response.json()
-        }).then((data) => {
-            console.log(data)
-        });
 
+    axios.get('http://localhost:3000/api/products').
+        then((response) => {
+            console.log(response.data);
+        })
 
     return (
         <>
